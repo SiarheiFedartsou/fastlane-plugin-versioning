4f3a2354bb40ee1f951584c140373ec2b7ac96e4
Siarhei Fiedartsou <siarhei.fedartsou@gmail.com>
2016-06-03 22:24:00 +0300

    rakefile configured

--- spec/get_info_plist_path_spec.rb
+++ spec/get_info_plist_path_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe Fastlane::Actions::GetInfoPlistPathAction do
   describe "Get Info.plist Path Integration" do
+    
     before do
       # Create test folder
       FileUtils.mkdir_p(test_path)
