5beb27a218c5cc99cc7348a1a6445b71c8e9ee53
Lauren Yeiser <lauren@thedyrt.com>
2018-09-12 18:35:56 -0700

    Add support for reading Bitrise CI build numbers

--- lib/fastlane/plugin/versioning/actions/ci_build_number.rb
+++ lib/fastlane/plugin/versioning/actions/ci_build_number.rb
@@ -38,6 +38,10 @@ module Fastlane
           return ENV['BITBUCKET_BUILD_NUMBER']
         end
 
+        if ENV.key?('BITRISE_BUILD_NUMBER')
+          return ENV['BITRISE_BUILD_NUMBER']
+        end
+
         if ENV.key?('BUDDYBUILD_BUILD_NUMBER')
           return ENV['BUDDYBUILD_BUILD_NUMBER']
         end
