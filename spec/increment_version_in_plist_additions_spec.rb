52b8a64430833948f0bb533b43ff78f039733648
Ryan C. Payne <rpayne-oss@bullittsystems.com>
2024-09-05 17:43:16 -0400

    To make it all work…

--- spec/increment_version_in_plist_additions_spec.rb
+++ spec/increment_version_in_plist_additions_spec.rb
@@ -16,7 +16,9 @@ describe Fastlane::Actions::IncrementVersionNumberInPlistAction do
 
     def current_xcodeproj_version_number
       Fastlane::FastFile.new.parse("lane :test do
-        get_version_number_from_xcodeproj
+        get_version_number_from_xcodeproj(
+          scheme: 'versioning_fixture_project'
+        )
       end").runner.execute(:test)
     end
 
