8e41ffbbcd9df6036e495c982bb90dd5bf9d3bad
Ryan C. Payne <rpayne-oss@bullittsystems.com>
2024-09-05 17:57:52 -0400

    Revert some of the madness

--- spec/increment_version_number_in_xcodeproj_spec.rb
+++ spec/increment_version_number_in_xcodeproj_spec.rb
@@ -14,17 +14,15 @@ describe Fastlane::Actions::IncrementVersionNumberInXcodeprojAction do
 
       version = Fastlane::FastFile.new.parse("lane :test do
         get_version_number_from_xcodeproj(
-          xcodeproj: '#{xcodeproj_path}'        
+          xcodeproj: '#{xcodeproj_path}'
           )
       end").runner.execute(:test)
       version
     end
 
-    def current_scheme_version
+    def current_version
       version = Fastlane::FastFile.new.parse("lane :test do
-        get_version_number_from_xcodeproj(
-          scheme: 'versioning_fixture_project'
-        )
+        get_version_number_from_xcodeproj
       end").runner.execute(:test)
       version
     end
@@ -38,73 +36,55 @@ describe Fastlane::Actions::IncrementVersionNumberInXcodeprojAction do
 
     it "should set explicitly provided version number to xcodeproj" do
       result = Fastlane::FastFile.new.parse("lane :test do
-        increment_version_number_in_xcodeproj(
-          scheme: 'versioning_fixture_project',
-          version_number: '1.9.4'
-        )
+        increment_version_number_in_xcodeproj(version_number: '1.9.4')
       end").runner.execute(:test)
 
-      expect(current_scheme_version).to eq("1.9.4")
+      expect(current_version).to eq("1.9.4")
       expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::VERSION_NUMBER]).to eq("1.9.4")
     end
 
     it "should bump patch version by default and set it to xcodeproj" do
       result = Fastlane::FastFile.new.parse("lane :test do
-        increment_version_number_in_xcodeproj(
-          scheme: 'versioning_fixture_project'
-        )
+        increment_version_number_in_xcodeproj
       end").runner.execute(:test)
 
-      expect(current_scheme_version).to eq("0.0.2")
+      expect(current_version).to eq("0.0.2")
       expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::VERSION_NUMBER]).to eq("0.0.2")
     end
 
     it "should bump patch version and set it to xcodeproj" do
       result = Fastlane::FastFile.new.parse("lane :test do
-        increment_version_number_in_xcodeproj(
-          scheme: 'versioning_fixture_project',
-          bump_type: 'patch'
-        )
+        increment_version_number_in_xcodeproj(bump_type: 'patch')
       end").runner.execute(:test)
 
-      expect(current_scheme_version).to eq("0.0.2")
+      expect(current_version).to eq("0.0.2")
       expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::VERSION_NUMBER]).to eq("0.0.2")
     end
 
     it "should bump minor version and set it to xcodeproj" do
       result = Fastlane::FastFile.new.parse("lane :test do
-        increment_version_number_in_xcodeproj(
-          scheme: 'versioning_fixture_project',
-          bump_type: 'minor'
-        )
+        increment_version_number_in_xcodeproj(bump_type: 'minor')
       end").runner.execute(:test)
 
-      expect(current_scheme_version).to eq("0.1.0")
+      expect(current_version).to eq("0.1.0")
       expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::VERSION_NUMBER]).to eq("0.1.0")
     end
 
     it "should omit zero in patch version if omit_zero_patch_version is true" do
       result = Fastlane::FastFile.new.parse("lane :test do
-        increment_version_number_in_xcodeproj(
-          scheme: 'versioning_fixture_project',
-          bump_type: 'minor',
-          omit_zero_patch_version: true
-        )
+        increment_version_number_in_xcodeproj(bump_type: 'minor', omit_zero_patch_version: true)
       end").runner.execute(:test)
 
-      expect(current_scheme_version).to eq("0.1")
+      expect(current_version).to eq("0.1")
       expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::VERSION_NUMBER]).to eq("0.1")
     end
 
     it "should bump major version and set it to xcodeproj" do
       result = Fastlane::FastFile.new.parse("lane :test do
-        increment_version_number_in_xcodeproj(
-          scheme: 'versioning_fixture_project', 
-          bump_type: 'major'
-        )
+        increment_version_number_in_xcodeproj(bump_type: 'major')
       end").runner.execute(:test)
 
-      expect(current_scheme_version).to eq("1.0.0")
+      expect(current_version).to eq("1.0.0")
       expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::VERSION_NUMBER]).to eq("1.0.0")
     end
 
@@ -113,7 +93,7 @@ describe Fastlane::Actions::IncrementVersionNumberInXcodeprojAction do
         increment_version_number_in_xcodeproj(bump_type: 'major', version_source: 'appstore')
       end").runner.execute(:test)
 
-      expect(current_scheme_version).to eq("3.0.0")
+      expect(current_version).to eq("3.0.0")
       expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::VERSION_NUMBER]).to eq("3.0.0")
     end
 
@@ -151,14 +131,14 @@ describe Fastlane::Actions::IncrementVersionNumberInXcodeprojAction do
 
     it "should increment the project level version" do
       expect(current_project_level_version).to eq("0.1.0")
-      
+
       xcodeproj_path = "/tmp/fastlane/tests/fastlane/xcodeproj/versioning_fixture_project_at_project_level.xcodeproj"
 
       Fastlane::FastFile.new.parse("lane :test do
         increment_version_number_in_xcodeproj(
           xcodeproj: '#{xcodeproj_path}'
-        )    
-      end").runner.execute(:test)  
+        )
+      end").runner.execute(:test)
 
       expect(current_project_level_version).to eq("0.1.1") # this was 0.1.0, and has now had a patch bump
     end
